f6ee7afa7600372f26c61546032d3d1bcc91df8e
KEREMSglm <keremsaglam@live.com>
2026-06-27 03:33:53 +0300

    3. commit msg

---
+++ main.rs
@@ -0,0 +1,7 @@
+// src/main.rs
+mod matematik;
+
+fn main() {
+    let sonuc = matematik::topla(2, 3);
+    println!("{}", sonuc);
+}
---
+++ shareable.rs
@@ -0,0 +1,4 @@
+// src/matematik.rs
+pub fn topla(a: i32, b: i32) -> i32 {
+    a + b
+}
